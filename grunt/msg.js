da3c9311d2ee1066e3a7609d4d1179fc89238c1e
Simon Kemmerling <simon.kemmerling@brandung.de>
2016-02-23 15:01:42 +0100

    Update packages and optimized error messages

--- grunt/msg.js
+++ grunt/msg.js
@@ -11,8 +11,8 @@ Msg.warnConfig = function () {
 	grunt.log.writeln('***              ERROR             ***'['red']);
 	grunt.log.writeln('**************************************'['red']);
 	grunt.log.error(['Project has been not initialized!'['red']]);
-	grunt.log.error(['Please run the init task and choose'['red']]);
-	grunt.log.error(['your system: `grunt project:init`'['red']]);
+	grunt.log.writeln(['Please run the init task and choose'['red']]);
+	grunt.log.writeln(['your system: `grunt project:init`'['red']]);
 	grunt.log.writeln('**************************************\n'['red']);
 	grunt.fail.warn('No \'system\' defined!');
 };
